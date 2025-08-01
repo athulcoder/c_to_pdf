9e75157e2d4e04e2c55d2c02c4d7ad3459e5b6ac
athulcoder <athul.sin.cos.tan@gmail.com>
2025-08-01 17:49:44 +0530

    breaking bug fixed

--- generate_pdf.py
+++ generate_pdf.py
@@ -36,7 +36,8 @@ def generate_pdf(c_file_path, student_name, roll_no, date_str, output_text, outp
         leading=12,
         spaceBefore=0,
         spaceAfter=0,
-        alignment=TA_LEFT
+        alignment=TA_LEFT,
+         wordWrap='CJK',
     )
 
 
